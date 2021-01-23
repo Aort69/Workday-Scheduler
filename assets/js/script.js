2033c149ae4e4777106c5f6a97099735629096b7
Allec <allec.ortega69@Gmail.com>
2021-01-23 16:54:01 -0700

    in process of setting up local storage

--- assets/js/script.js
+++ assets/js/script.js
@@ -56,67 +56,18 @@ function timedColor() {
     else if (hour < currentHour) {
       timeBlocks[i].classList.add("past");
     }
-
-    console.log(currentHour);
   }
 }
-timedColor();
-//   var tc = new Date();
-// var hourCompare = tc.getHours();
-// if (hourCompare <= 9 && hourCompare >= 10) {
-//   //day time
-//   $('.time').css("background-color", "red");
-// } else {
-//   //night time
-//   $('.time').css("background-color", "green");
-// }
-// if (hourCompare <= 10 && hourCompare >= 11) {
-//   //day time
-//   $('.time').css("background-color", "red");
-// } else {
-//   //night time
-//   $('.time').css("background-color", "green");
-// }
-// if (hourCompare <= 11 && hourCompare >= 12) {
-//   //day time
-//   $('.time').css("background-color", "red");
-// } else {
-//   //night time
-//   $('.time').css("background-color", "green");
-// }
-// if (hourCompare <= 12 && hourCompare >= 13) {
-//   //day time
-//   $('.time').css("background-color", "red");
-// } else {
-//   //night time
-//   $('.time').css("background-color", "green");
-// }
-// if (hourCompare <= 13 && hourCompare >= 14) {
-//   //day time
-//   $('.time').css("background-color", "red");
-// } else {
-//   //night time
-//   $('.time').css("background-color", "green");
-// }
-// if (hourCompare <= 14 && hourCompare >= 15) {
-//   //day time
-//   $('.time').css("background-color", "red");
-// } else {
-//   //night time
-//   $('.time').css("background-color", "green");
-// }
-// if (hourCompare <= 15 && hourCompare >= 16) {
-//   //day time
-//   $('.time').css("background-color", "red");
-// } else {
-//   //night time
-//   $('.time').css("background-color", "green");
-// }
-// if (hourCompare <= 16 && hourCompare >= 17) {
-//   //day time
-//   $('.time').css("background-color", "red");
-// }
-// }
+
+// click function
+$( ".saveBtn" ).click(function() {
+  var userInput = $(this).siblings('.colorMe').text().trim()
+  console.log(userInput)
+});
+
+
+
+
 
 timedColor();
 showDate();
